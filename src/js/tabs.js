0ecbcae8c9926c0c5f128230bfac5eb2d2610379
Pixeloided <pixeloidsemail@gmail.com>
2022-08-25 22:32:06 -0700

    update got

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,1 +1,12 @@
-document.querySelectorAll("[data-tab-target]").forEach((a) => {a.addEventListener("click", () => {document.querySelectorAll(`[data-tab-group='${a.dataset.tabGroup}']`).forEach((a) => {a.classList.remove("active-tab");a.classList.remove("tab-active");});document.querySelector(a.dataset.tabTarget).classList.add("active-tab");a.classList.add("tab-active");});});
+document.querySelectorAll("[data-tab-target]").forEach((a) => {
+  a.addEventListener("click", () => {
+    document
+      .querySelectorAll(`[data-tab-group='${a.dataset.tabGroup}']`)
+      .forEach((a) => {
+        a.classList.remove("active-tab");
+        a.classList.remove("tab-active");
+      });
+    document.querySelector(a.dataset.tabTarget).classList.add("active-tab");
+    a.classList.add("tab-active");
+  });
+});
